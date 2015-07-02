94e16123e58f55f55934ca993e9e7fbc37257b33
karan sharma <karan.sharma516@yahoo.com>
2015-07-02 11:54:58 -0400

    Complate the midterm

---
+++ COMP2007-Midterm/Courses/Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Entity;
+using COMP2007_Midterm;
+
+namespace COMP2007_Midterm.Courses
+{
+    public partial class Default : System.Web.UI.Page
+    {
+		protected COMP2007_Midterm.DefaultConnection _db = new COMP2007_Midterm.DefaultConnection();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        // Model binding method to get List of Course entries
+        // USAGE: <asp:ListView SelectMethod="GetData">
+        public IQueryable<COMP2007_Midterm.Course> GetData()
+        {
+            return _db.Courses;
+        }
+    }
+}
+
---
+++ COMP2007-Midterm/Courses/Insert.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Entity;
+using COMP2007_Midterm;
+
+namespace COMP2007_Midterm.Courses
+{
+    public partial class Insert : System.Web.UI.Page
+    {
+		protected COMP2007_Midterm.DefaultConnection _db = new COMP2007_Midterm.DefaultConnection();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // This is the Insert method to insert the entered Course item
+        // USAGE: <asp:FormView InsertMethod="InsertItem">
+        public void InsertItem()
+        {
+            using (_db)
+            {
+                var item = new COMP2007_Midterm.Course();
+
+                TryUpdateModel(item);
+
+                if (ModelState.IsValid)
+                {
+                    // Save changes
+                    _db.Courses.Add(item);
+                    _db.SaveChanges();
+
+                    Response.Redirect("Default");
+                }
+            }
+        }
+
+        protected void ItemCommand(object sender, FormViewCommandEventArgs e)
+        {
+            if (e.CommandName.Equals("Cancel", StringComparison.OrdinalIgnoreCase))
+            {
+                Response.Redirect("Default");
+            }
+        }
+    }
+}
